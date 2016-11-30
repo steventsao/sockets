80e32da912e27bf46402f1425715959066b7becc
steven <steventsao713@gmail.com>
2016-11-29 22:23:54 -0800

    Send index.html as default

--- server/index.js
+++ server/index.js
@@ -1,29 +1,17 @@
 const express = require('express');
 const fetch = require('isomorphic-fetch');
-const CHANNEL = require('./api.wire.js');
-
+const api = require('./api.wire.js');
+const path = require('path');
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(express.static(path.resolve('dist')));
 
-app.get('/youtube', (req, res) => {
-  fetch('http://www.youtube.com')
-    .then((data) => {
-      res.send(data);
-    });
-});
+var indexHtml = fs.readFileSync(path.resolve('index.html')).toString();
 
-app.get('/channel', (req, res) => {
-  fetch(CHANNEL.GET)
-    .then((data) => {
-      res.send(data);
-    });
+app.get('*', (req, res) => {
+  res.send(indexHtml);
 });
-// Mimic the following API call to get user ID
-// http://johnnythetank.github.io/youtube-channel-name-converter/
-// https://www.googleapis.com/youtube/v3/channels?key=AIzaSyDsGZDPI461UR5JvTysAqv7PW7HSzj50KU&forUsername=nigahiga&part=id
 
 app.listen(3000, () => {
   console.log('App is listening on port 3000');
