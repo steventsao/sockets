a0ee7ff8866547c5b1ea0b5f4fa68d7c5cf5369b
steven <steventsao713@gmail.com>
2017-01-26 23:02:16 -0800

    - Enable socket in server

--- server/index.js
+++ server/index.js
@@ -4,20 +4,50 @@ const request = require('request');
 const api = require('./api.wire.js');
 const path = require('path');
 const fs = require('fs');
-const app = express();
 const bodyParser = require('body-parser');
+const socket = require('socket.io')
+
+const app = express();
+const http = require('http').Server(app);
+const io = socket(http);
 
 app.use(bodyParser.json());
 app.use(express.static(path.resolve('public')));
 
-var indexHtml = fs.readFileSync(path.resolve('index.html')).toString();
+// var indexHtml = fs.readFileSync(path.resolve('index.html')).toString();
 
 var instance = axios.create({
   baseURL: api.channel.GET
 });
 
 app.get('/', (req, res) => {
-  res.send(indexHtml);
+  res.send(`
+  <!doctype html>
+    <html>
+      <head>
+        <title>Socket.IO chat</title>
+        <style>
+          * { margin: 0; padding: 0; box-sizing: border-box; }
+          body { font: 13px Helvetica, Arial; }
+          form { background: #000; padding: 3px; position: fixed; bottom: 0; width: 100%; }
+          form input { border: 0; padding: 10px; width: 90%; margin-right: .5%; }
+          form button { width: 9%; background: rgb(130, 224, 255); border: none; padding: 10px; }
+          #messages { list-style-type: none; margin: 0; padding: 0; }
+          #messages li { padding: 5px 10px; }
+          #messages li:nth-child(odd) { background: #eee; }
+        </style>
+      </head>
+      <body>
+        <ul id="messages"></ul>
+        <form action="">
+          <input id="m" autocomplete="off" /><button>Send</button>
+        </form>
+        <script src="/socket.io/socket.io.js"></script>
+        <script>
+          var socket = io();
+        </script>
+      </body>
+    </html>`);
 });
 
 app.get('/channel', (req, res) => {
@@ -26,6 +56,10 @@ app.get('/channel', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(3000, () => {
   console.log('App is listening on port 3000');
 });
