7f2328f064996782e62a60aa066b9f70c0628be5
steven <steventsao713@gmail.com>
2017-02-25 22:21:46 -0800

    Animate Timer

--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -13,22 +13,32 @@ class Timer extends React.Component<ITimerProps, ITimerState> {
     return "Time's up! All options are disabled.";
   }
 
-  // NOTE: do not setState within componentDidUpdate to avoid recursive calls
-  render() {
+  renderDescription() {
     if (this.props.seconds) {
-      return (
-        <div>
-          {this.props.seconds} Seconds Left...
-        </div>
-      );
+      return <div>
+        {this.props.seconds} Seconds Left...
+      </div>;
     } else {
-      return (
-        <div>
+      return <div>
           {this.getTimeoutDescription()}
           <button onClick={this.props.restart}>Restart</button>
-        </div>
-      );
+        </div>;
     }
+  }
+  // NOTE: do not setState within componentDidUpdate to avoid recursive calls
+  render() {
+    return (
+      <div>
+        <div className="clock-container">
+          <div className="clock">
+            <div className="clock-frame">
+              <div className={`clock-needle ${this.props.seconds ? "animate" : ""}`}></div>
+            </div>
+          </div>
+        </div>
+          <p>{this.renderDescription()}</p>
+      </div>
+    );
   }
 }
 
