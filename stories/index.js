3d859217ea44f24f8686f6e7beeb31162f989fa0
steven <steventsao713@gmail.com>
2017-03-15 01:15:41 -0700

    Start name card component in storybook

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
 import Button from './Button';
+import NameCard from './NameCard';
 import Welcome from './Welcome';
 
 storiesOf('Welcome', module)
@@ -15,3 +16,8 @@ storiesOf('Button', module)
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
   ));
+
+storiesOf('Name Card', module)
+  .add('Using input', () => <NameCard userName="Steven"/>)
+  .add('Default Name', () => <NameCard />)
+
